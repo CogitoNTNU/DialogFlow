eaeddd7079ed8a2f2c009332e5956d187a7c21ba
gitGut01 <silaseei@hotmail.com>
2019-10-03 22:08:40 +0200

    Remove ingredients intent: list test

--- src/main/java/com/example/MyActionsApp.kt
+++ src/main/java/com/example/MyActionsApp.kt
@@ -61,9 +61,11 @@ class MyActionsApp : DialogflowApp() {
         val rb = ResourceBundle.getBundle("resources")
         val user = request.user
 
-        val rm_i = request.getParameter("rm_i").toString()
+        val rm_i = request.getParameter("rm_i") as List<String>
 
-        responseBuilder.add("Removed $rm_i, u little B*tch")
+        val r = rm_i[0]
+
+        responseBuilder.add("Removed $r, u little B*tch")
 
         LOGGER.info("Fjern ingredients slutt")
         return responseBuilder.build()
