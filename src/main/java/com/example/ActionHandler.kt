9acb400d4c16993aae9f0a6a87e3052a234e0736
MarTorb <marius.torbjornsen@gmail.com>
2019-10-24 18:05:06 +0200

    Add removePizza function

--- src/main/java/com/example/ActionHandler.kt
+++ src/main/java/com/example/ActionHandler.kt
@@ -1,4 +1,30 @@
 package com.example
 
 class ActionHandler {
+
+
+    fun removePizza(types : IntArray, amount : IntArray): String {
+
+        var response  = ""
+
+        val order: Order = orderManager[request]
+
+        if (order.pizzas.size > 0) {
+            for (pizza: Pizza in order.pizzas) {
+                for (i in types.indices) {
+                    if (pizza.nr == types[i]) {
+                        for (j in 0 until amount[i]) {
+                            order.removePizza(pizza)
+                        }
+                        response += "Fjernet" + amount[i] + pizza.name
+                    }
+                }
+            }
+        }
+        else {
+            response = "Du har ikke bestilt noen pizzaer enda."
+        }
+        return response
+    }
+
 }
